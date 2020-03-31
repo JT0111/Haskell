3731f70de2679e1a7e961a097d2de86e6eb12011
JT0111 <jt1g19@soton.ac.uk>
2020-03-31 09:53:33 +0000

    added comments explaining pretty much everything

--- FirstFile.hs
+++ FirstFile.hs
@@ -1,2 +1,21 @@
-smallTriangles = [(a, b, c)| a <- [1..10], b <- [1..a], c <- [1..10]]
-rightSmallTriangles = [(a, b, c) | (a, b, c) <- smallTriangles, a*a + b*b == c*c]
+rightTriangles = [(a, b, c) |  a <- [1..10],  b <- [1..a], c <- [1..100], a^2 + b^2 == c^2, a + b > c ]
+
+ifRight :: (Num a, Ord a) => (a, a, a) -> Bool
+ifRight (a, b, c) = if a*a + b*b == c*c && a + b > c then True else False
+
+lotsOfHearts = take 100(cycle "<3")
+sameAsAboveButAsASetOfStrings = replicate 50 "<3"
+
+jts :: Integral a => [a] -> [[Char]]
+jts xs = [ if x < 10 then "J" else "T" | x <- xs, odd x] 
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z']] 
+ 
+evenSubsetsMembers :: [[Int]] -> [[Int]]
+evenSubsetsMembers xxs = [ [ x | x <- xs, even x ] | xs <- xxs]
+
+numbers = zip [1, 2, 3] ["one", "two", "three"]
+
+increasedLength :: (Foldable t, Fractional a1) => t a2 -> a1
+increasedLength xs = fromIntegral (length xs) + 3.2
