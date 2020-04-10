6314c0b49fe3eb4ffb6cf21bbdfba023a5201003
JT0111 <jt1g19@soton.ac.uk>
2020-04-10 16:56:38 +0200

    Started messing with more exercises but it's  kinda hard

--- Lab4.hs
+++ Lab4.hs
@@ -1,4 +1,4 @@
---Exercise 1
+--Exercise 1 (not finished)
 --Decide if all elements of a list satisfy a predicate
 all' :: (a -> Bool) -> [a] -> Bool
 all' f xs = foldr (\x acc -> if f x then acc else False) True xs
@@ -9,8 +9,12 @@ any' f xs = foldr (\x acc -> if f x then True else acc) False xs
 
 --Select the initial elements from a list while they satisfy a predicate
 --takeWhile' :: (a -> Bool) -> [a] -> [a]
---takeWhile'   
+--takeWhile' p xs = 
 
 --Exercise 2
 dec2Int :: [Int] -> Int
 dec2Int xs = foldl (\acc x -> (10 * acc) + x) 0 xs
+
+--Exercise 4
+
+int2bin xs = unfold (==1) h t x
